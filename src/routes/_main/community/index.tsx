64c9193561af44d46658cac0a20c2d5b4d30f65c
bloody_bit <riccardo.sibani@gmail.com>
2025-12-02 18:48:17 +0100

    making sure that all the prompts are returned before implementing pagination

--- src/routes/_main/community/index.tsx
+++ src/routes/_main/community/index.tsx
@@ -79,7 +79,7 @@ export function Feed() {
     excludeUsed: false,
     pagination: {
       page: currentPage,
-      size: 10,
+      size: 30,
     },
     community: selectedCommunity,
     tokenCollections:
